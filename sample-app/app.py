e8edf3d74a79d6c4c4c63657ed779f743560ac4c
Sam Donche <sam.donche@mustrysolutions.com>
2026-06-02 08:42:50 +0200

    add actual debug functionality

--- sample-app/app.py
+++ sample-app/app.py
@@ -30,4 +30,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    debug = os.environ.get("FLASK_DEBUG", "0") == "1"
+    app.run(host="0.0.0.0", port=5050, debug=debug)
